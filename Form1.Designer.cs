5f84ab1777cfd29f1532be9dbc4b04499d2be0f9
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-10-11 10:21:10 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace _10._1_Rafael
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.trb1 = new System.Windows.Forms.TrackBar();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(245, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Postavljena vrijednost je:";
+            // 
+            // trb1
+            // 
+            this.trb1.Location = new System.Drawing.Point(57, 98);
+            this.trb1.Name = "trb1";
+            this.trb1.Size = new System.Drawing.Size(862, 45);
+            this.trb1.TabIndex = 1;
+            this.trb1.Scroll += new System.EventHandler(this.trb1_Scroll);
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tb1.Location = new System.Drawing.Point(543, 241);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 31);
+            this.tb1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 594);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.trb1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trb1;
+        private System.Windows.Forms.TextBox tb1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10._1_Rafael
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trb1_Scroll(object sender, EventArgs e)
+        {
+            trb1.Minimum = 0;
+            trb1.Maximum = 100;
+            trb1.LargeChange = 1;
+
+            tb1.Text = trb1.Value.ToString();
+        }
+    }
+}
